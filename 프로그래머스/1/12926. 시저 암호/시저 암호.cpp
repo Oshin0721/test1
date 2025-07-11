4ee806b4151d020dec0cd5878064d6d9727ee873
신동하 <ehdgk3210@naver.com>
2025-07-11 09:46:53 +0900

    [level 1] Title: 시저 암호, Time: 0.11 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s, int n) {
+    string answer = "";
+    
+    for (char c : s)
+    {
+        if (c == ' ')
+        {
+            answer += c;
+        }
+        else if (c >= 'A' && c <= 'Z')
+        {
+            answer += (c - 'A' + n) % 26 + 'A';
+        }
+        else if (c>= 'a' && c <= 'z')
+        {
+            answer += (c - 'a' + n) % 26 +'a';
+        }
+    }
+    
+    return answer;
+}
